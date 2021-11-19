df62f627f2b390dce6ee559da4de598327e02081
Greypixel <greypixel@gpxl.tech>
2021-11-19 16:27:43 +0000

    Update favicon and loading

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,7 +3,8 @@ import React, { FC } from "react";
 import sdog from "../svg/sdog.svg";
 
 const LoadingSpinner: FC<{}> = ({}) => {
-  return <img src={sdog} className="animate-spin w-4 h-4" />;
+  return <span className="text-lightGrey font-extralight">Loading...</span>;
+  // return <img src={sdog} className="animate-spin w-4 h-4" />;
 };
 
 export default LoadingSpinner;
